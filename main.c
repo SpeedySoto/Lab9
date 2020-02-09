a326cd09e0dc6a07ee64e320273f0211002c89fb
Hector Soto Jr <59711305+SpeedySoto@users.noreply.github.com>
2020-02-09 14:21:24 -0800

    Lab 9 Exercise 2

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 /*	Partner(s) Name & E-mail: David Wilson
  *	Lab Section: 023
- *	Assignment: Lab #9  Exercise #1 
+ *	Assignment: Lab #9  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *	
  *	I acknowledge all content contained herein, excluding template 
@@ -11,23 +11,21 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void set_PWM(double frequency)
- {
+void set_PWM(double frequency) {
 	static double current_frequency; 
-	if (frequency != current_frequency)
-	 {
+	if (frequency != current_frequency) {
 		if (!frequency) { TCCR0B &= 0x08; } 
 		else { TCCR0B |= 0x03; } 
+		
 
 		if (frequency < 0.954) { OCR0A = 0xFFFF; }
 		
-
 		else if (frequency > 31250) { OCR0A = 0x0000; }
-
+		
 		else { OCR0A = (short)(8000000 / (128 * frequency)) - 1; }
 
 		TCNT0 = 0; 
-		current_frequency = frequency; 
+		current_frequency = frequency;
 	}
 }
 
@@ -46,85 +44,110 @@ void PWM_off()
 	TCCR0B = 0x00;
 }
 
-enum States{Hold, CState, DState, EState}state;
+enum States{CState, DState, EState, FState, GState, AState, BState, C5State}state;
+
 
 unsigned char buttonPress = 0x00;
 
-void button_Tick()
-{
+const double notes[8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+unsigned char i = 0x00;
+
+void button_Tick(){
 	buttonPress = ~PINA & 0x07;
 	switch(state)
 	{ 
-		case Hold:
+		case CState:
 		if(buttonPress == 1)
+		state = EState;
+		else
+		state = CState;
+		break;
+		case DState:
+		if(!(buttonPress == 1))
+		state = CState;
+		else
+		state = DState;
+		break;
+		case EState:
+		if(buttonPress == 1)
+		state = EState;
+		else
+		state = FState;
+		break;
+		case FState:
+		if(buttonPress == 2)
 		{
-			state = CState;
-		}
-		else if(buttonPress == 2)
-		{
-			state = DState;
+			if(i < 7)
+			i++;
+			state = GState;
 		}
 		else if(buttonPress == 4)
 		{
-			state = EState;
+			if(i > 0)
+			i--;
+			state = BState;
 		}
-		else{
-			state = Hold;
-		}
+		else if(buttonPress == 1)
+		state = DState;
+		else
+		state = FState;
 		break;
-		case CState:
-		if(buttonPress == 1)
-		{
-			state = CState;
-		}
-		else{
-			state = Hold;
-		}
+		case GState:
+		state = AState;
 		break;
-		case DState:
+		case AState:
 		if(buttonPress == 2)
-		{
-			state = DState;
-		}
-		else{
-			state = Hold;
-		}
+		state = AState;
+		else
+		state = FState;
 		break;
-		case EState:
+		case BState:
+		state = C5State;
+		break;
+		case C5State:
 		if(buttonPress == 4)
-		{
-			state = EState;
-		}
-		else{
-			state = Hold;
-		}
+		state = C5State;
+		else
+		state = FState;
 		break;
 	}
+	
+	
 	switch(state)
-	{
-		case Hold:
-		set_PWM(0);
-		break;
+	{ 
 		case CState:
-		set_PWM(261.63);
+		PWM_off();
 		break;
 		case DState:
-		set_PWM(293.66);
 		break;
 		case EState:
-		set_PWM(329.63);
+		PWM_on();
 		break;
+		case FState:
+		break;
+		case GState:
+		set_PWM(notes[i]);
+		break;
+		case AState:
+		break;
+		case BState:
+		set_PWM(notes[i]);
+		break;
+		case C5State:
+		break;
+
 	}
 }
 
 
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF; // A input initialized to 0xFF
-	DDRB = 0xFF; PORTB = 0x00; // B output initialized to 0x00
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRB = 0xFF; PORTB = 0x00; 
 	PWM_on();
-	state = Hold;
+	state = CState;
 	while(1){
 		button_Tick();
 	}
-} 
+}
+
